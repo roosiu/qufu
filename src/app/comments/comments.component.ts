381f606a0185228d9a725c0160d933ccdcebcba0
roosiu <roosiu@gmail.com>
2024-02-26 14:07:43 +0100

    comments section upgrade

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -2,18 +2,41 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatModule } from '../mat/mat.module';
 import { SearchService } from '../services/search.service';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [MatModule, CommonModule],
+  imports: [MatModule, CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css',
 })
 export class CommentsComponent implements OnInit {
   @Input() id: any;
   comments: any;
-  constructor(private searchService: SearchService) {}
+  commentForm: FormGroup;
+  newComment: FormControl;
+  logged = false;
+  name: any;
+  constructor(
+    private searchService: SearchService,
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.newComment = new FormControl('', [Validators.maxLength(450)]);
+    this.commentForm = new FormGroup({
+      newComment: this.newComment,
+    });
+  }
+
   /// oceny
   getStarsArray(rating: number): number[] {
     const fullStarsCount = Math.floor(rating);
@@ -25,6 +48,11 @@ export class CommentsComponent implements OnInit {
     return Array(emptyStarsCount).fill(0);
   }
   ngOnInit(): void {
+    this.authService.GetLogged().subscribe((bol: boolean) => {
+      this.logged = bol;
+      this.name = localStorage.getItem('name');
+    });
+
     if (this.id) {
       this.searchService
         .getJsonData('?commentsByRecipeId=' + this.id)
